d2a6ba20278ac71420f87cdda1650edb2193fc3b
revirasaude <65733263+revirasaude@users.noreply.github.com>
2020-05-21 16:52:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ function carregarConsultores(){
 function inicializarMapa(){
 	//Carregar Mapa
 	mymap = L.map('mapid'/*,{dragging: !L.Browser.mobile}*/).setView([51.505, -0.09], 6);
-	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmV2aXJhc2F1ZGUiLCJhIjoiY2thaDY3cjl1MGdoYzJ4cWhteHg3NzNleiJ9.uAR_3bDLrVH4rQ-O10vdMQ', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
